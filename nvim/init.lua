42e40c4c1ba494f636c2a1fc5b24076f92550ee8
alem633 <mannaalessio885@duck.com>
2025-04-12 14:40:34 +0200

    neovim

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require("alessio")
+vim.o.background = "light" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[set relativenumber]])
+
+vim.cmd([[set shiftwidth=2]])
+vim.cmd([[set autoindent]])
+vim.cmd([[set smartindent]])
