7ba381bf917126b74ec06ed3799968ac5085e9b1
alem633 <mannaalessio885@gmail.com>
2025-04-12 18:33:51 +0200

    fish

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
+
+# Created by `pipx` on 2025-03-04 17:03:55
+set PATH $PATH /home/alessio/.local/bin
