ba1a8d3e2014b69f423204b3c98d86e9d04f6031
alem633 <mannaalessio885@gmail.com>
2025-05-27 22:29:26 +0200

    neovim markdown fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,14 @@ vim.cmd([[set relativenumber]])
 vim.cmd([[set shiftwidth=2]])
 vim.cmd([[set autoindent]])
 vim.cmd([[set smartindent]])
+
+-- Enables linebreak and wrap in Markdown files
+vim.api.nvim_create_augroup("MarkdownSettings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  group = "MarkdownSettings",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
