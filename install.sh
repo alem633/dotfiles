61b8d38cd0d49f1e27720cee6a0874b2ca5af584
alem633 <mannaalessio885@gmail.com>
2025-09-14 19:14:00 +0200

    auto install fonts

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo "installing fonts"
+mkdir -p ~/.fonts
+cd ~/.fonts
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+tar xvf JetBrainsMono.tar.xz
+rm JetBrainsMono
+
+cd
+
 cp -r fish hypr kitty nvim sway waybar wofi ~/.config
 cp .tmux.conf ~/
