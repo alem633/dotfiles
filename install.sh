17c593dfedfe5c2274db5b2fe7d001bbd6e90acd
alem633 <mannaalessio885@gmail.com>
2025-09-14 19:22:54 +0200

    removed nvim

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+cp -r fish hypr kitty sway waybar wofi ~/.config
+cp .tmux.conf ~/
+
 echo "installing fonts"
-mkdir -p ~/.fonts
-cd ~/.fonts
+mkdir -p ~/.fonts/JetBrainsNerd
+cd ~/.fonts/JetBrainsNerd
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
 tar xvf JetBrainsMono.tar.xz
-rm JetBrainsMono
-
-cd
-
-cp -r fish hypr kitty nvim sway waybar wofi ~/.config
-cp .tmux.conf ~/
--- nvim/lua/alessio/init.lua
+++
@@ -1,7 +0,0 @@
-require("alessio.remap")
-vim.cmd([[set relativenumber]])
-
-vim.cmd([[set shiftwidth=2]])
-vim.cmd([[set autoindent]])
-vim.cmd([[set smartindent]])
-
